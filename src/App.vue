c523a32d559808bc39a612c6b27af4a3797f9746
nanami <kom_nana5102@icloud.com>
2021-04-04 20:43:51 +0900

    update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <header id="nav">
+    <div class="nav-container">
+      <div class="nav-left">
+        <div>nanami</div>
+        <!-- <a href="https://fontmeme.com/ja/font-signature/"
+          ><img
+            class="nav-left-img"
+            src="https://fontmeme.com/permalink/210330/86265904da868351312d6fcc83be3480.png"
+            alt="font-signature"
+            border="0"
+        /></a> -->
+      </div>
+      <div class="nav-right">
+        <router-link to="/" class="nav-right-item">Home</router-link>
+        <router-link to="/skills" class="nav-right-item">Skills</router-link>
+        <router-link to="/works" class="nav-right-item">Works</router-link>
+        <router-link to="/playground" class="nav-right-item"
+          >Playground</router-link
+        >
+      </div>
+    </div>
+  </header>
   <router-view />
 </template>
 
@@ -11,12 +29,12 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  // text-align: center;
   color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+  // padding: 30px;
 
   a {
     font-weight: bold;
@@ -26,5 +44,29 @@
       color: #42b983;
     }
   }
+}
+
+header {
+  position: fixed;
+  width: 100%;
+  height: 100px;
+  z-index: 8;
+  top: 0px;
+  left: 0px;
+}
+
+.nav-container {
+  padding: 0 80px;
+  height: 100%;
+  z-index: 9;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.nav-right-item {
+  margin-left: 20px;
 }
 </style>
