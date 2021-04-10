47c13718e79deb9a5fa33c73b728ff58624d1db4
nanami <kom_nana5102@icloud.com>
2021-04-10 23:50:03 +0900

    smallfix

--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="container">
+  <div class="con">
     <Dayjs />
-  </div>
-  <div class="container">
-    <Btn msg="click me" hello />
+    <div class="btn">
+      <Btn msg="click me" hello />
+    </div>
   </div>
 </template>
 
@@ -16,3 +16,22 @@ export default defineComponent({
   components: { Dayjs, Btn },
 });
 </script>
+
+<style lang="scss" scoped>
+.con {
+  text-align: center;
+  width: 1000px;
+  margin: 80px auto;
+  @media screen and (max-width: 1200px) {
+    width: 700px;
+  }
+  @media screen and (max-width: 768px) {
+    width: 500px;
+  }
+}
+
+.btn {
+  width: 240px;
+  margin: 0 auto;
+}
+</style>
--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container" style="margin-top: 80px">
     <h1>This is a skills page</h1>
     <h1>coming soon...</h1>
   </div>
